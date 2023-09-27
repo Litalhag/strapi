88c1d80b5bf28b01ed432cf44b599ea61dd7f04d
Litalhag <litalhag@gmail.com>
2023-09-27 17:32:01 +0300

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Sidebar from './components/Sidebar'
+import Submenu from './components/Submenu'
+
+import './index.css'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  )
+}
+export default App
